81c5321a00376213fb7b43229b76852e51120e9b
LeosLudv05 <leosludva2016@outlook.cz>
2025-04-28 09:38:17 +0200

    Commit 1

--- fibo.py
+++ fibo.py
@@ -1,5 +1,15 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
+    n = int(input("Počet členů Fibonacciho posloupnosti: "))
+    seq = [recursive_nth_fibo(num) for num in range(n + 1)]
+    print(seq)
     pass
 
 
